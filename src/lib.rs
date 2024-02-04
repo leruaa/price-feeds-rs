485b67567fe41093d0722698e169124caee09219
leruaa <aurelien.catinon@gmail.com>
2024-02-04 07:10:29 +0000

    Add tower Service

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 mod fallback;
 pub mod feeds;
 mod price_feed;
+mod service;
 
 pub use fallback::Fallback as FallbackFeed;
 pub use price_feed::PriceFeed;
+pub use service::UsdPriceFeedService;
---
+++ src/service.rs
@@ -0,0 +1,42 @@
+use std::{pin::Pin, sync::Arc, task::Poll};
+
+use alloy_primitives::Address;
+use anyhow::Error;
+use bigdecimal::BigDecimal;
+use futures::Future;
+use tower::Service;
+
+use crate::PriceFeed;
+
+pub struct UsdPriceFeedService {
+    inner: Arc<dyn PriceFeed>,
+}
+
+impl UsdPriceFeedService {
+    pub fn new(inner: Arc<dyn PriceFeed>) -> Self {
+        Self { inner }
+    }
+}
+
+impl Service<Address> for UsdPriceFeedService {
+    type Response = BigDecimal;
+
+    type Error = Error;
+
+    type Future = Pin<Box<dyn Future<Output = Result<Self::Response, Self::Error>> + Send>>;
+
+    fn poll_ready(
+        &mut self,
+        _cx: &mut std::task::Context<'_>,
+    ) -> std::task::Poll<Result<(), Self::Error>> {
+        Poll::Ready(Ok(()))
+    }
+
+    fn call(&mut self, token: Address) -> Self::Future {
+        let inner = self.inner.clone();
+
+        let fut = async move { inner.usd_price(token).await };
+
+        Box::pin(fut)
+    }
+}
