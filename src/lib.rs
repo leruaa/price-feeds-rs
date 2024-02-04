db433a512c31b16cab52d889e875af7abc16ed84
leruaa <aurelien.catinon@gmail.com>
2024-02-04 07:17:41 +0000

    Add a optional feature for tower

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,12 @@
 mod fallback;
 pub mod feeds;
 mod price_feed;
+
+#[cfg(feature = "tower")]
 mod service;
 
 pub use fallback::Fallback as FallbackFeed;
 pub use price_feed::PriceFeed;
+
+#[cfg(feature = "tower")]
 pub use service::UsdPriceFeedService;
